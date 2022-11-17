165bc6e77008164f47c9591278f51765c42312d3
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2022-11-17 18:02:10 +0100

    Refactor keydown eventlistener

--- Calculator_v2/script.js
+++ Calculator_v2/script.js
@@ -13,115 +13,57 @@ const checkbox = document.querySelector('#theme input[type="checkbox"]');
 checkbox.addEventListener('change', (event) => themeSwitch(event));
 
 function themeSwitch(event) {
-    if (event.target.checked) {
-        document.documentElement.setAttribute('theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('theme', 'light');
-    }
+  if (event.target.checked) {
+    document.documentElement.setAttribute('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('theme', 'light');
+  }
 }
 
 function showHistory() {
-    if (logger.getAttribute("name") === "show") {
-        logger.setAttribute("name", "hide");
-    } else {
-        logger.setAttribute("name", "show");
-    }
-    return;
+  if (logger.getAttribute("name") === "show") {
+    logger.setAttribute("name", "hide");
+  } else {
+    logger.setAttribute("name", "show");
+  }
+  return;
 }
 
 document.body.addEventListener('keydown', function (event) {
-
-    switch (event.key) {
-        case '.':
-            btnNumbers.forEach(btn => {
-                if (btn.innerHTML === '.') display.addNumber(btn.innerHTML);
-            });
-            break;
-        case '0':
-            btnNumbers.forEach(btn => {
-                if (btn.innerHTML === '0') display.addNumber(btn.innerHTML);
-            });
-            break;
-        case '1':
-            btnNumbers.forEach(btn => {
-                if (btn.innerHTML === '1') display.addNumber(btn.innerHTML);
-            });
-            break;
-        case '2':
-            btnNumbers.forEach(btn => {
-                if (btn.innerHTML === '2') display.addNumber(btn.innerHTML);
-            });
-            break;
-        case '3':
-            btnNumbers.forEach(btn => {
-                if (btn.innerHTML === '3') display.addNumber(btn.innerHTML);
-            });
-            break;
-        case '4':
-            btnNumbers.forEach(btn => {
-                if (btn.innerHTML === '4') display.addNumber(btn.innerHTML);
-            });
-            break;
-        case '5':
-            btnNumbers.forEach(btn => {
-                if (btn.innerHTML === '5') display.addNumber(btn.innerHTML);
-            });
-            break;
-        case '6':
-            btnNumbers.forEach(btn => {
-                if (btn.innerHTML === '6') display.addNumber(btn.innerHTML);
-            });
-            break;
-        case '7':
-            btnNumbers.forEach(btn => {
-                if (btn.innerHTML === '7') display.addNumber(btn.innerHTML);
-            });
-            break;
-        case '8':
-            btnNumbers.forEach(btn => {
-                if (btn.innerHTML === '8') display.addNumber(btn.innerHTML);
-            });
-            break;
-        case '9':
-            btnNumbers.forEach(btn => {
-                if (btn.innerHTML === '9') display.addNumber(btn.innerHTML);
-            });
-            break;
-        case '+':
-            btnOperators.forEach(btn => {
-                if (btn.innerHTML === '+') display.chooseOperation(btn.value);
-            });
-            break;
-        case '-':
-            btnOperators.forEach(btn => {
-                if (btn.innerHTML === '-') display.chooseOperation(btn.value);
-            });
-            break;
-        case '*':
-            btnOperators.forEach(btn => {
-                if (btn.innerHTML === '*') display.chooseOperation(btn.value);
-            });
-            break;
-        case '/':
-            btnOperators.forEach(btn => {
-                if (btn.innerHTML === '/') display.chooseOperation(btn.value);
-            });
-            break;
-        case '%':
-            btnOperators.forEach(btn => {
-                if (btn.innerHTML === '%') display.chooseOperation(btn.value);
-            });
-            break;
-        case 'Enter' : case '=' :
-            btnOperators.forEach(btn => {
-                if (btn.innerHTML === '=') display.chooseOperation(btn.value);
-            });
-            break;
-        case 'Backspace':
-            display.delete();
-            break;
-        case 'Delete':
-            display.deleteAll();
-            break;
-    }
+  let eventKey = event.key;
+  switch (eventKey) {
+    case 'Backspace':
+      display.delete();
+      break;
+    case 'Delete':
+      display.deleteAll();
+      break;
+    case eventKey:
+      if (!isNaN(eventKey)) {
+        display.addNumber(eventKey);
+      } else {
+        if (eventKey === 'Enter' || eventKey === '=') {
+          display.chooseOperation('equal')
+          return;
+        } else if (eventKey === '+') {
+          display.chooseOperation('add')
+          return;
+        } else if (eventKey === '-') {
+          display.chooseOperation('substract')
+          return;
+        } else if (eventKey === '*') {
+          display.chooseOperation('multiply')
+          return;
+        } else if (eventKey === '/') {
+          display.chooseOperation('divide')
+          return;
+        } else if (eventKey === '%') {
+          display.chooseOperation('percent')
+        } else if (eventKey === '.' || eventKey === ',') {
+          display.addNumber(eventKey);
+          return;
+        } else return;
+      }
+      break;
+  }
 });
