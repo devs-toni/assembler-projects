21cfd34372d047a8a09dbd8a47152b0786cec5d3
Antonio Rufino <devs.toni@gmail.com>
2022-11-17 11:33:24 +0100

    added keyboard keys to html buttons

--- Calculator_v2/script.js
+++ Calculator_v2/script.js
@@ -13,11 +13,11 @@ const checkbox = document.querySelector('#theme input[type="checkbox"]');
 checkbox.addEventListener('change', (event) => themeSwitch(event));
 
 function themeSwitch(event) {
-  if (event.target.checked) {
-    document.documentElement.setAttribute('theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('theme', 'light');
-  }
+    if (event.target.checked) {
+        document.documentElement.setAttribute('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('theme', 'light');
+    }
 }
 
 function showHistory() {
@@ -29,7 +29,94 @@ function showHistory() {
     return;
 }
 
-function pushKey(event) {
-    var x = event.key;
-    alert(x);
-  }
+document.body.addEventListener('keydown', function (event) {
+
+    switch (event.key) {
+        case '0':
+            btnNumbers.forEach(btn => {
+                if (btn.innerHTML === '0') display.addNumber(btn.innerHTML);
+            });
+            break;
+        case '1':
+            btnNumbers.forEach(btn => {
+                if (btn.innerHTML === '1') display.addNumber(btn.innerHTML);
+            });
+            break;
+        case '2':
+            btnNumbers.forEach(btn => {
+                if (btn.innerHTML === '2') display.addNumber(btn.innerHTML);
+            });
+            break;
+        case '3':
+            btnNumbers.forEach(btn => {
+                if (btn.innerHTML === '3') display.addNumber(btn.innerHTML);
+            });
+            break;
+        case '4':
+            btnNumbers.forEach(btn => {
+                if (btn.innerHTML === '4') display.addNumber(btn.innerHTML);
+            });
+            break;
+        case '5':
+            btnNumbers.forEach(btn => {
+                if (btn.innerHTML === '5') display.addNumber(btn.innerHTML);
+            });
+            break;
+        case '6':
+            btnNumbers.forEach(btn => {
+                if (btn.innerHTML === '6') display.addNumber(btn.innerHTML);
+            });
+            break;
+        case '7':
+            btnNumbers.forEach(btn => {
+                if (btn.innerHTML === '7') display.addNumber(btn.innerHTML);
+            });
+            break;
+        case '8':
+            btnNumbers.forEach(btn => {
+                if (btn.innerHTML === '8') display.addNumber(btn.innerHTML);
+            });
+            break;
+        case '9':
+            btnNumbers.forEach(btn => {
+                if (btn.innerHTML === '9') display.addNumber(btn.innerHTML);
+            });
+            break;
+        case '+':
+            btnOperators.forEach(btn => {
+                if (btn.innerHTML === '+') display.chooseOperation(btn.value);
+            });
+            break;
+        case '-':
+            btnOperators.forEach(btn => {
+                if (btn.innerHTML === '-') display.chooseOperation(btn.value);
+            });
+            break;
+        case '*':
+            btnOperators.forEach(btn => {
+                if (btn.innerHTML === '*') display.chooseOperation(btn.value);
+            });
+            break;
+        case '/':
+            btnOperators.forEach(btn => {
+                if (btn.innerHTML === '/') display.chooseOperation(btn.value);
+            });
+            break;
+        case '%':
+            btnOperators.forEach(btn => {
+                if (btn.innerHTML === '%') display.chooseOperation(btn.value);
+            });
+            break;
+        case 'Enter' : case '=' :
+            btnOperators.forEach(btn => {
+                if (btn.innerHTML === '=') display.chooseOperation(btn.value);
+            });
+            break;
+        case 'Backspace':
+            display.delete();
+            break;
+        case 'Delete':
+            display.deleteAll();
+            break;
+    }
+});
