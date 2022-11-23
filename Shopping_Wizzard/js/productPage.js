3a44cf9bfe6fb5c9fc8d32a10e57f5e01f9dc4cc
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2022-11-23 16:42:46 +0100

    Add styles and functionality to product page

--- Shopping_Wizzard/js/productPage.js
+++ Shopping_Wizzard/js/productPage.js
@@ -1,8 +1,13 @@
 const thumbnails = document.querySelector('.product-thumbnails');
 const thumbnailsChildren = thumbnails.children
 const image = document.querySelector('.product-image');
+const productPageForm = document.getElementById('product-page-form');
 
-getThumbnails('black');
+function submitForm(e) {
+    e.preventDefault();
+    console.log(e.target.battery.value);
+    console.log(e.target.color.value);
+}
 
 function getThumbnails(color) {
     for (let i = 0; i < 4; i++) {
@@ -10,7 +15,7 @@ function getThumbnails(color) {
             `<img 
                 onclick="changeImage(${i + 1}, '${color}')" 
                 src="./assets/products/${color}/thumbnails/thumb-${color}-${i + 1}.png" 
-                width="50px" 
+                width="80px" 
                 alt="${color}-${i + 1}" 
                 image-number="${i + 1}"
             >`;
@@ -28,3 +33,7 @@ function changeColor(color) {
     image.children[0].alt = color;
     getThumbnails(color);
 }
+
+
+getThumbnails('black');
+productPageForm && productPageForm.addEventListener('submit', (e) => submitForm(e));
