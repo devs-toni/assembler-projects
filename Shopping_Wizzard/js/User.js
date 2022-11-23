66991aed8bd3ea3f573d8f10f525d4cbbd5c1089
Pablo Herrero <pablohgb@gmail.com>
2022-11-23 15:25:31 +0100

    pssw validation corrected

--- Shopping_Wizzard/js/User.js
+++ Shopping_Wizzard/js/User.js
@@ -55,6 +55,45 @@ class User {
     submitLogin = (e) => {
         e.preventDefault();
     
+        if(this.password === "") {  
+            document.getElementById("pswdMessage").innerHTML = "**Fill the password please!";  
+            return false;  
+         }  
+          
+        //minimum password length validation  
+         if(this.password.length < 8) {  
+            document.getElementById("pswdMessage").innerHTML = "**Password length must be at least 8 characters";  
+            return false;  
+         }  
+         
+         //contains number validation
+        if(this.password.search(/[0-9]/) < 0){
+            document.getElementById("pswdMessage").innerHTML = "**Password must contain at least 1 number";  
+        }
+     
+     //contains uppercase validation
+        if(this.password.search(/[A-Z]/) < 0){
+            document.getElementById("pswdMessage").innerHTML = "**Password must contain at least 1 uppercase letter";  
+        }
+     //contains lowercase validation
+        if(this.password.search(/[a-z]/) < 0){
+            document.getElementById("pswdMessage").innerHTML = "**Password must contain at least 1 lowercase letter";  
+        }
+     //maximum length of password validation  
+        if(this.password.length > 20) {  
+           document.getElementById("pswdMessage").innerHTML = "**Password length must not exceed 20 characters";  
+           return false;  
+        } else {  
+           alert("Password is correct");  
+        }  
+
+         let pw1 = document.getElementById("passwordConfirm");  
+         if(pw1 != this.password)  
+         {   
+           alert("Passwords did not match");  
+         } else {  
+           alert("Password created successfully");  
+         }  
     }
 
     assignFormValues = () => {
@@ -69,53 +108,6 @@ class User {
         this.isRegularAdress = regularAddress.checked;
     }
 
-    verifyPassword (){
-        let pw = document.getElementById("password").value;  
-        //check empty password field  
-        if(pw == "") {  
-           document.getElementById("pswdMessage").innerHTML = "**Fill the password please!";  
-           return false;  
-        }  
-         
-       //minimum password length validation  
-        if(pw.length < 8) {  
-           document.getElementById("pswdMessage").innerHTML = "**Password length must be atleast 8 characters";  
-           return false;  
-        }  
-        
-        //contains number validation
-    if(pw.search(/[0-9]/) < 0){
-        document.getElementById("pswdMessage").innerHTML = "**Password must contain at least 1 number";  
-    }
-    
-    //contains uppercas validation
-    if(pw.search(/[A-Z]/) < 0){
-        document.getElementById("pswdMessage").innerHTML = "**Password must contain at least 1 uppercase letter";  
-    }
-    //contains lowercase validation
-    if(pw.search(/[a-z]/) < 0){
-        document.getElementById("pswdMessage").innerHTML = "**Password must contain at least 1 lowercase letter";  
-    }
-    //maximum length of password validation  
-        if(pw.length > 20) {  
-           document.getElementById("pswdMessage").innerHTML = "**Password length must not exceed 20 characters";  
-           return false;  
-        } else {  
-           alert("Password is correct");  
-        }  
-      }  
-    
-    
-    confirmPassword (){
-        let pw1 = document.getElementById("password");  
-        let pw2 = document.getElementById("passwordConfirm");  
-        if(pw1 != pw2)  
-        {   
-          alert("Passwords did not match");  
-        } else {  
-          alert("Password created successfully");  
-        }  
-    }
 }
 
 
