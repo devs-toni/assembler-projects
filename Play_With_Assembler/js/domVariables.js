307181e101614d2130c665771f24a561df18b241
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2022-12-01 17:06:19 +0100

    Add DOM variables

--- Play_With_Assembler/js/domVariables.js
+++ Play_With_Assembler/js/domVariables.js
@@ -1,8 +1,12 @@
 /***************** Choose user *****************/
+const userDiv = document.getElementById('userDiv');
 const userForm = document.getElementById('chooseNameForm');
 
 /***************** Game *****************/
+const gameDiv = document.getElementById('gameDiv');
+
 
 /***************** Finish *****************/
+const finishDiv = document.getElementById('finishDiv');
 
 /***************** User scores*****************/
