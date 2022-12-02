24503570da4423fe0c9421cdaeaeb4ca73a3ccab
Joe Alt <joejoyalt@gmail.com>
2022-12-02 15:16:13 +0100

    FULL structure of hangman html css js

--- Play_With_Assembler/js/Game.js
+++ Play_With_Assembler/js/Game.js
@@ -38,3 +38,93 @@ class Game {
         //Actualizar scores
     }
 }
+
+var programming_languages = [
+    "almond",
+    "banana",
+    "carrot",
+    "cashew",
+    "cherry",
+    "citron",
+    "garlic",
+    "lentel",
+    "lichee",
+    "orange",
+    "papaya",
+    "peanut",
+    "tomato",
+    "walnut"
+]
+
+let answer = '';
+let maxWrong = 7;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
+function randomWord() {
+    answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+}
+
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+    if (answer.indexOf(chosenLetter) >= 0) {
+        guessedWord();
+        checkIfGameWon();
+    } else if (answer.indexOf(chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes();
+        checkIfGameLost();
+        updateHangmanPicture();
+    }
+}
+
+function updateHangmanPicture() {
+    document.getElementById('hangmanPic').src = 'assets/images/' + mistakes + '.jpg';
+}
+
+
+function checkIfGameWon() {
+    if (wordStatus === answer) {
+        gameDiv.classList.remove('game-window-active');
+        finishDiv.classList.add('game-window-active');
+        document.getElementById('keyboard').innerHTML = 'You Won!!!';
+    }
+}
+
+function checkIfGameLost() {
+    if (mistakes === maxWrong) {
+        document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+        document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+    }
+}
+
+function guessedWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+    document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    document.getElementById('hangmanPic').src = './images/0.jpg';
+
+    randomWord();
+    guessedWord();
+    updateMistakes();
+    generateButtons();
+    location.reload();
+}
+
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+randomWord();
+generateButtons();
+guessedWord();
