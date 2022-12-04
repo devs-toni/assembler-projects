d008ff27c1e546d83680a11cbe1eec40ee220ca5
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2022-12-04 20:15:40 +0100

    Add variables

--- Play_With_Assembler/js/domVariables.js
+++ Play_With_Assembler/js/domVariables.js
@@ -4,9 +4,26 @@ const userForm = document.getElementById('chooseNameForm');
 
 /***************** Game *****************/
 const gameDiv = document.getElementById('gameDiv');
-
+const wordSpotlight = document.getElementById('wordSpotlight');
+const hangmanPic = document.getElementById('hangmanPic');
+const keyboard = document.getElementById('keyboard');
+const alphabet = document.querySelectorAll('[data-target="alphabet"]');
 
 /***************** Finish *****************/
-const finishDiv = document.getElementById('finishDiv');
+const finishMessage = document.getElementById('finishMessage');
+const finishMessageAnswer = document.getElementById('finishMessageAnswer');
+const finishMessageResult = document.getElementById('finishMessageResult');
+const finishDivTotal = document.getElementById('finishDivTotal');
+const playAgainButton = document.getElementById('playAgainButton');
 
 /***************** User scores*****************/
+const userScores = document.getElementById('userScores');
+const userScoresList = document.getElementById('userScoresList');
+const userScoresDiv = document.getElementById('userScoresDiv');
+const resetScores = document.getElementById('resetScores');
+const userScoreName = document.getElementById('userScoreName');
+const userScoreTime = document.getElementById('userScoreTime');
+
+
+/***************** LocalStorage ***************/
+let historyGameLS = localStorage.getItem('game-history');
