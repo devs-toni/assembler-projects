702c721d2226718bc77d724b3d24d635fe1efa4e
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2022-12-12 16:24:36 +0100

    Add HTML structure

--- Blog_With_API/src/js/main.js
+++ Blog_With_API/src/js/main.js
@@ -1,3 +1,8 @@
-import '../scss/styles.scss';
+import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap';
 
+const myModal = document.querySelector("#postId");
+
+myModal.addEventListener('click', (e) => {
+    console.log(e.target.id);
+})
