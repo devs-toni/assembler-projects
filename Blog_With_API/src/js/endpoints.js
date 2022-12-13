caf5d08a7b88b02191c7825c73412eba7281f15d
Antonio Rufino <devs.toni@gmail.com>
2022-12-13 10:01:54 +0100

    db restaured

--- Blog_With_API/src/js/endpoints.js
+++ Blog_With_API/src/js/endpoints.js
@@ -1,9 +1,10 @@
 //All Called Local Endpoints
 const localPostsEndpoint = 'http://localhost:3000/posts';
 const localUsersEndpoint = 'http://localhost:3000/users';
+const localCommentsEndpoint = 'http://localhost:3000/comments';
 
 
 //All Called API Endpoints
 const usersEndpoint = 'https://jsonplaceholder.typicode.com/users';
 const postsEndpoint = 'https://jsonplaceholder.typicode.com/posts';
-const commentsEndpoint = 'https://jsonplaceholder.typicode.com/commefdsnts';
+const commentsEndpoint = 'https://jsonplaceholder.typicode.com/comments';
