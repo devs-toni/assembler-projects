89e4a7cf04db33c358b64b5c93c330c5d20d9f15
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2022-12-13 17:56:18 +0100

    Add toogle to show comments

--- Blog_With_API/src/js/main.js
+++ Blog_With_API/src/js/main.js
@@ -1,2 +1,15 @@
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap';
+
+const commentsButton = document.getElementById('commentsButton');
+const commentsDiv = document.getElementById('commentsDiv');
+
+commentsButton.addEventListener('click', () => toogleComments())
+
+function toogleComments() {
+  if (commentsDiv.classList.contains('d-none')) {
+    commentsDiv.classList.remove('d-none');
+  } else {
+    commentsDiv.classList.add('d-none');
+  }
+}
