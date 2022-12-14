82e4e3c5a19d847bb9aa9abd3e671d27e599c298
Antonio Rufino <devs.toni@gmail.com>
2022-12-14 14:07:21 +0100

    solve bug endpoints

--- Blog_With_API/src/js/controller.js
+++ Blog_With_API/src/js/controller.js
@@ -3,20 +3,21 @@ let idToRemovePost;
 
 const loadPosts = () => {
   let list = document.getElementById('posts');
-
-  http().get(getPostsAPI).then((data) => {
+  http().get(postsAPI).then((data) => {
     data.forEach(async post => {
-      let cloneList = list.children[0].cloneNode(true);
-      cloneList.id = `post${post.id}`;
-      cloneList.children[0].onclick = (e) => openModalPost(e);
-      cloneList.children[0].textContent = post.title;
-      cloneList.children[2].id = `trashPost${post.id}`;
-      cloneList.children[2].children[0].id = `trashPost${post.id}`;
-      cloneList.children[2].onclick = (e) => waitingIdToRemovePost(e);
-      await http().get(getCommentsByPostAPI(post.id)).then((comments) => {
-        cloneList.children[1].textContent = comments.length;
-      });
-      list.appendChild(cloneList);
+      if (post) {
+        let cloneList = list.children[0].cloneNode(true);
+        cloneList.id = `post${post.id}`;
+        cloneList.children[0].onclick = (e) => openModalPost(e);
+        cloneList.children[0].textContent = post.title;
+        cloneList.children[2].id = `trashPost${post.id}`;
+        cloneList.children[2].children[0].id = `trashPost${post.id}`;
+        cloneList.children[2].onclick = (e) => waitingIdToRemovePost(e);
+        await http().get(commentsByPostAPI(post.id)).then((comments) => {
+          cloneList.children[1].textContent = comments.length;
+        });
+        list.appendChild(cloneList);
+      }
     });
   }).then(() => list.children[0].remove());
 }
@@ -24,14 +25,14 @@ const loadPosts = () => {
 async function openModalPost(e) {
   let idUser;
   let id = e.target.parentElement.id.replace('post', '');
-  await http().get(getPostByIdAPI(id)).then(res => {
+  await http().get(postByIdAPI(id)).then(res => {
     if (res) {
       idUser = res.userId;
       document.getElementById('modalContent').textContent = res.body;
       document.getElementById('showModalLabel').textContent = res.title;
     }
   });
-  http().get(getUserByIdAPI(id)).then(data => {
+  http().get(userByIdAPI(id)).then(data => {
     if (data) {
       document.getElementById('modalUser').innerHTML =
         `<p>Email - ${data.email}</p><p>Username - ${data.username}</p>`
--- Blog_With_API/src/js/endpoints.js
+++ Blog_With_API/src/js/endpoints.js
@@ -1,15 +1,15 @@
 const remoteAPI = false;
-const remote = 'http://localhost:3000';
-const local = 'https://jsonplaceholder.typicode.com'
+const local = 'http://localhost:3000/';
+const remote = 'https://jsonplaceholder.typicode.com/'
 
 /* Users */
-const usersAPI = `http://${remoteAPI ? remote : local}/users`; //non-used
-const userByIdAPI = id => `http://${remoteAPI ? remote : local}/users/${id}`;
+const usersAPI = `${remoteAPI ? remote : local}users`; //non-used
+const userByIdAPI = id => `${remoteAPI ? remote : local}users/${id}`;
 
 /* Posts */
-const postsAPI = `http://${remoteAPI ? remote : local}/posts`;
-const postByIdAPI = id => `http://${remoteAPI ? remote : local}/posts/${id}`;
+const postsAPI = `${remoteAPI ? remote : local}posts`;
+const postByIdAPI = id => `${remoteAPI ? remote : local}posts/${id}`;
 
 /* Comments */
-const commentsAPI = 'http://localhost:3000/comments'; //non-used
-const commentsByPostAPI = id => `http://${remoteAPI ? remote : local}/comments?postId=${id}`;
+const commentsAPI = `${remoteAPI ? remote : local}comments`; //non-used
+const commentsByPostAPI = id => `${remoteAPI ? remote : local}comments?postId=${id}`;
