b223eb10ae61acf0fb3f39ccf758e3d7b95bac31
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2022-12-15 16:58:28 +0100

    Refactor modals

--- Blog_With_API/src/js/main.js
+++ Blog_With_API/src/js/main.js
@@ -10,6 +10,7 @@ const editModalTitle = document.getElementById('editModalLabel');
 const editTitle = document.getElementById('editTitle');
 const editBody = document.getElementById('editBody');
 const list = document.getElementById('posts');
+const editModalFooter = document.getElementById('editModalFooter');
 
 // Charge posts from DB
 
@@ -17,22 +18,19 @@ const loadPosts = async () => {
 
   await http().get(postsAPI).then((data) => {
     data.forEach(async post => {
-      let cloneList = list.children[0].cloneNode(true);
-      cloneList.id = `post${post.id}`;
-      cloneList.children[0].onclick = openModalPost;
-      cloneList.children[0].textContent = post.title;
-      cloneList.children[2].id = `editPost${post.id}`;
-      cloneList.children[2].children[0].id = `editPost${post.id}`;
-      cloneList.children[2].onclick = openEditPost;
-      cloneList.children[3].id = `trashPost${post.id}`;
-      cloneList.children[3].children[0].id = `trashPost${post.id}`;
-      cloneList.children[3].onclick = (e) => {
-        finalRemovePostButton.setAttribute('remove-id', e.target.id.replace('trashPost', ''));
+      if (post) {
+        let cloneList = list.children[0].cloneNode(true);
+        cloneList.id = `post${post.id}`;
+        cloneList.children[0].onclick = openModalPost;
+        cloneList.children[0].textContent = post.title;
+        cloneList.children[2].id = `editPost${post.id}`;
+        cloneList.children[2].children[0].id = `editPost${post.id}`;
+        cloneList.children[2].onclick = openEditPost;
+        await http().get(commentsByPostAPI(post.id)).then((comments) => {
+          cloneList.children[1].textContent = comments.length;
+        });
+        list.appendChild(cloneList);
       }
-      await http().get(commentsByPostAPI(post.id)).then((comments) => {
-        cloneList.children[1].textContent = comments.length;
-      });
-      list.appendChild(cloneList);
     });
   }).then(() => list.removeChild(list.children[0]));
 }
@@ -40,43 +38,60 @@ const loadPosts = async () => {
 // Configure modal when you open it.
 
 async function openModalPost(e) {
-
+  
   commentsDiv.innerHTML = '';   //Reset data inside modal
   let idUser;
   let id = e.target.parentElement.id.replace('post', '');
+
   await http().get(postByIdAPI(id)).then(result => {
     if (result) {
       idUser = result.userId;
       document.getElementById('modalContent').textContent = result.body;
       document.getElementById('showModalLabel').textContent = result.title;
-    }
+    } 
   });
-  await http().get(userByIdAPI(id)).then(result => {
+
+  await http().get(userByIdAPI(idUser)).then((result) => {
     if (result) {
       document.getElementById('modalUser').innerHTML =
-        `<p>Email - ${result.email}</p><p>Username - ${result.username}</p>`
+        `<p>Email - ${result.email}</p><p>Username - ${result.username}</p>`;
     }
   });
+  
   await http().get(commentsByPostAPI(id)).then((result) => {
-    result.forEach((comment) => {
-      commentsDiv.innerHTML += `
-        <div>
-          <h4 class='border-top pb-2 pt-4'>${comment.name}</h4>
-          <p class='pb-2'>${comment.body}</p>
-          <p class='pb-2'>${comment.email}</p>
-        </div>
-      `;
-    });
+    if (result) {
+      result.forEach((comment) => {
+        commentsDiv.innerHTML += `
+          <div>
+            <h4 class='border-top pb-2 pt-4'>${comment.name}</h4>
+            <p class='pb-2'>${comment.body}</p>
+            <p class='pb-2'>${comment.email}</p>
+          </div>
+        `;
+      });
+    }
   });
 }
 
 async function openEditPost(e) {
   finalEditPostButton.setAttribute('edit-id', e.target.id.replace('editPost', ''));
   let idToEditPost = finalEditPostButton.getAttribute('edit-id');
-  editModalTitle.innerText += `: ID(${idToEditPost})`;
-  await http().get(postByIdAPI(idToEditPost)).then(result => {
+
+  await http().get(postByIdAPI(idToEditPost))
+  .then(result => {
     editTitle.value = result.title;
     editBody.value = result.body;
+  })
+  .then(() => {
+      http().get(userByIdAPI(idToEditPost)).then(result => {
+      editModalTitle.innerText = `${result.name} @${result.username}`;
+  })
+  .then(() => {
+    editModalFooter.children[0].id = `trashPost${idToEditPost}`;
+    editModalFooter.children[0].onclick = (e) => {
+      finalRemovePostButton.setAttribute('remove-id', e.target.id.replace('trashPost', ''));
+    }
+  });
   });
 }
 
@@ -88,7 +103,6 @@ async function removePost() {
     document.getElementById(`post${idToRemovePost}`).remove();
   });
 }
-
 
 async function editPost(e) {
   e.preventDefault();
